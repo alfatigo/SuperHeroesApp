e293194679f5f015f7447a16504732059de2d9e6
Alexander Reyes <alexreyes64r@gmail.com>
2023-11-26 13:01:39 -0400

    SuperHeroe C#

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+var superman = new SuperHeroe();
+
+
+superman.Id = 1;
+superman.Name = "Batman";
+superman.secretIdentity = "Bruce Wayne";
+superman.City = "Gotrham City";
+superman .Fly = false;
+superman.Abilities = new[] {"Smart","Rich","superior Technology"};
+class SuperHeroe
+{
+
+    public int Id;
+    public string Name;
+    public string secretIdentity;
+    public string City;
+    public string[] Abilities;
+    public bool Fly;
+
+}
